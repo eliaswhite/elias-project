30561531ba715517d860f12fa8a67627e950246b
elias <souzabrancoelias8@gmail.com>
2022-05-04 15:05:57 -0300

    E

--- src/components/MenuLateral.vue
+++ src/components/MenuLateral.vue
@@ -1,12 +1,54 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      times: [
+        { time: "Atlético Mineiro", flag: "@/assets/img/atleticomg.png" },
+        {
+          time: "Avai",
+          flag: "@/assets/img/avai-fc-logo-escudo-1 1.png",
+        },
+        { time: "Atlético Mineiro", flag: "@/assets/img/amer.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/at.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/cap.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/cea.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/cor.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/fla.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/flu.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/for.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/go.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/i 1.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/inter.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/juv.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/pal.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/red.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/san.png" },
+        { time: "Atlético Mineiro", flag: "@/assets/img/sao.png" },
+      ],
+    };
+  },
+};
 </script>
 <template>
   <aside id="sidenav">
-    <img src="@/assets/img/atleticomg.png" /><br>
-    <img src="@/assets/img/avai-fc-logo-escudo-1 1.png" /><br>
-    <img src="@/assets/img/logo-cap 1.png" /><br>
-    <img src="@/assets/" alt="">
+    <img src="@/assets/img/atleticomg.png" />
+    <img src="@/assets/img/avai-fc-logo-escudo-1 1.png" />
+    <img src="@/assets/img/amer.png" />
+    <img src="@/assets/img/at.png" />
+    <img src="@/assets/img/cap.png" />
+    <img src="@/assets/img/cea.png" />
+    <img src="@/assets/img/cor.png" />
+    <img src="@/assets/img/fla.png" />
+    <img src="@/assets/img/flu.png" />
+    <img src="@/assets/img/for.png" />
+    <img src="@/assets/img/go.png" />
+    <img src="@/assets/img/i 1.png" />
+    <img src="@/assets/img/inter.png" />
+    <img src="@/assets/img/juv.png" />
+    <img src="@/assets/img/pal.png" />
+    <img src="@/assets/img/red.png" />
+    <img src="@/assets/img/san.png" />
+    <img src="@/assets/img/sao.png" />
   </aside>
 </template>
 <style></style>
