c86e721912bde129bd7b58ef60dfcf276ff56cc9
Elias <souzabrancoelias8@gmail.com>
2022-05-06 11:36:22 -0300

    Elias

--- src/components/MenuLateral.vue
+++ src/components/MenuLateral.vue
@@ -3,27 +3,73 @@ export default {
   data() {
     return {
       times: [
-        { time: "Atlético Mineiro", flag: "@/assets/img/atleticomg.png" },
+        {
+          time: "Atlético Mineiro",
+          flag: new URL("./img/atleticomg.png", import.meta.url).href,
+        },
         {
           time: "Avai",
-          flag: "@/assets/img/avai-fc-logo-escudo-1 1.png",
+          flag: new URL("./img/avai-fc-logo-escudo-1.png", import.meta.url)
+            .href,
         },
-        { time: "Atlético Mineiro", flag: "@/assets/img/amer.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/at.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/cap.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/cea.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/cor.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/fla.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/flu.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/for.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/go.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/i 1.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/inter.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/juv.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/pal.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/red.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/san.png" },
-        { time: "Atlético Mineiro", flag: "@/assets/img/sao.png" },
+        {
+          time: "América Mineiro",
+          flag: new URL("./img/amer.png", import.meta.url).href,
+        },
+        {
+          time: "Atlético Goianiense",
+          flag: new URL("./img/at.png", import.meta.url).href,
+        },
+        {
+          time: "Atlético paranaense",
+          flag: new URL("./img/cap.png", import.meta.url).href,
+        },
+        { time: "Ceara", flag: new URL("./img/cea.png", import.meta.url).href },
+        {
+          time: "Corinthians",
+          flag: new URL("./img/cor.png", import.meta.url).href,
+        },
+        {
+          time: "Flamengo",
+          flag: new URL("./img/fla.png", import.meta.url).href,
+        },
+        {
+          time: "Fluminense",
+          flag: new URL("./img/flu.png", import.meta.url).href,
+        },
+        {
+          time: "Fortaleza",
+          flag: new URL("./img/for.png", import.meta.url).href,
+        },
+        { time: "Goiás", flag: new URL("./img/go.png", import.meta.url).href },
+        {
+          time: "Cuiabá",
+          flag: new URL("./img/i 1.png", import.meta.url).href,
+        },
+        {
+          time: "Internacional",
+          flag: new URL("./img/inter.png", import.meta.url).href,
+        },
+        {
+          time: "Juventude",
+          flag: new URL("./img/juv.png", import.meta.url).href,
+        },
+        {
+          time: "Palmeiras",
+          flag: new URL("./img/pal.png", import.meta.url).href,
+        },
+        {
+          time: "Red Bull Bragantino",
+          flag: new URL("./img/red.png", import.meta.url).href,
+        },
+        {
+          time: "Santos",
+          flag: new URL("./img/san.png", import.meta.url).href,
+        },
+        {
+          time: "São Paulo",
+          flag: new URL("./img/sao.png", import.meta.url).href,
+        },
       ],
     };
   },
@@ -31,24 +77,12 @@ export default {
 </script>
 <template>
   <aside id="sidenav">
-    <img src="@/assets/img/atleticomg.png" />
-    <img src="@/assets/img/avai-fc-logo-escudo-1 1.png" />
-    <img src="@/assets/img/amer.png" />
-    <img src="@/assets/img/at.png" />
-    <img src="@/assets/img/cap.png" />
-    <img src="@/assets/img/cea.png" />
-    <img src="@/assets/img/cor.png" />
-    <img src="@/assets/img/fla.png" />
-    <img src="@/assets/img/flu.png" />
-    <img src="@/assets/img/for.png" />
-    <img src="@/assets/img/go.png" />
-    <img src="@/assets/img/i 1.png" />
-    <img src="@/assets/img/inter.png" />
-    <img src="@/assets/img/juv.png" />
-    <img src="@/assets/img/pal.png" />
-    <img src="@/assets/img/red.png" />
-    <img src="@/assets/img/san.png" />
-    <img src="@/assets/img/sao.png" />
+    <img
+      v-for="(time, i) of times"
+      :key="i"
+      :src="time.flag"
+      :title="time.time"
+    />
   </aside>
 </template>
 <style></style>
