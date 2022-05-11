619ef4fb3bc837d0e384b1bdc80ba2677cacabd5
Elias <souzabrancoelias8@gmail.com>
2022-05-11 15:07:51 -0300

    Elias

--- src/components/BlocoPrincipal.vue
+++ src/components/BlocoPrincipal.vue
@@ -4,16 +4,23 @@ export default {};
 <template>
   <main id="content">
     <div class="blocos">
-      <p>Resultados</p>
+      <p>Partidas</p>
       <div class="bloco-interno">
-        <p>fkkasjdkfjskda</p>
+        <p>
+          Avaí 2x1 Coritiba // Fortaleza 1x1 São Paulo <br />
+          Juventude 1x1 Internacional // Red Bull Bragantino 0x1 Corinthians
+          <br />
+          Santos 4x1 Cuiabá // Atlético-GO 0x1 Goiás <br />
+          Palmeiras 1x1 Fluminense // Flamengo 0x1 Botafogo <br />
+          Athletico-PR 1x0 Ceará // Atlético-MG 1x2 América-MG
+        </p>
       </div>
       <p>Noticias</p>
       <div class="bloco-interno">
         <p>djfkdajkfjakj</p>
       </div>
     </div>
-    <!-- <article class="block">  Resultados</article>
+    <!-- <article class="block">Partidas</article>
     <article class="block">Últimas Notícias</article> -->
   </main>
 </template>
